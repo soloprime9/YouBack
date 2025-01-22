970e4806445e9156b7463b89b271ea0a233355bd
soloprime9 <primeindia5750@gmail.com>
2025-01-23 04:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(express.json());
 
 app.use(cors ({
-    origin: "http://localhost:3000"
+    origin: "https://downloads-nine.vercel.app/"
 }))
 
 app.get("/", (req, res)=>{
